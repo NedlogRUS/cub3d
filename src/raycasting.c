ffc2a47855d18faa8b0bf34058f67db77e9819d1
apanikov <panikov.a.a@gmail.com>
2023-09-05 22:59:40 +0400

    norminetted raycasting

--- src/raycasting.c
+++ src/raycasting.c
@@ -6,36 +6,36 @@
 /*   By: apanikov <apanikov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 21:46:43 by apanikov          #+#    #+#             */
-/*   Updated: 2023/09/05 22:40:35 by apanikov         ###   ########.fr       */
+/*   Updated: 2023/09/05 22:59:03 by apanikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
 
-void floor_ceiling(t_data *c3d)
+void	floor_ceiling(t_data *c3d)
 {
-	int y;
-	int x;
+	int	y;
+	int	x;
 
 	y = 0;
-    while (y < SCREENHEIGHT / 2)
-    {
-        x = 0;
-        while (x < SCREENWIDTH)
-            c3d->image->addr[y * SCREENWIDTH + x++] = c3d->ceilingColor;
-        y++;
-    }
-    y = SCREENHEIGHT / 2;
-    while (y < SCREENHEIGHT)
-    {
-        int x = 0;
-        while (x < SCREENWIDTH)
-            c3d->image->addr[y * SCREENWIDTH + x++] = c3d->floorColor;
-        y++;
-    }
+	while (y < SCREENHEIGHT / 2)
+	{
+		x = 0;
+		while (x < SCREENWIDTH)
+			c3d->image->addr[y * SCREENWIDTH + x++] = c3d->ceilingColor;
+		y++;
+	}
+	y = SCREENHEIGHT / 2;
+	while (y < SCREENHEIGHT)
+	{
+		x = 0;
+		while (x < SCREENWIDTH)
+			c3d->image->addr[y * SCREENWIDTH + x++] = c3d->floorColor;
+		y++;
+	}
 }
 
-void determine_ray_intersects_distances(t_data *c3d)
+void	determine_ray_intersects_distances(t_data *c3d)
 {
 	if (c3d->rayDirX < 0)
 	{
@@ -59,7 +59,7 @@ void determine_ray_intersects_distances(t_data *c3d)
 	}
 }
 
-void hit_wall(t_data *c3d)
+void	hit_wall(t_data *c3d)
 {
 	while (c3d->hit == 0)
 	{
@@ -80,7 +80,7 @@ void hit_wall(t_data *c3d)
 	}
 }
 
-void calculating_parameters_rendering_wall(t_data *c3d)
+void	calculating_parameters_rendering_wall(t_data *c3d)
 {
 	if (c3d->side == 0)
 		c3d->perpWallDist = (c3d->sideDistX - c3d->deltaDistX);
@@ -102,7 +102,7 @@ void calculating_parameters_rendering_wall(t_data *c3d)
 	c3d->wallX -= floor(c3d->wallX);
 }
 
-void side_of_the_wall(t_data *c3d)
+void	side_of_the_wall(t_data *c3d)
 {
 	if (c3d->side == 0)
 	{
@@ -122,8 +122,8 @@ void side_of_the_wall(t_data *c3d)
 
 void	calculating_textured_rendering_wall(t_data *c3d, int x)
 {
-	int y;
-	
+	int	y;
+
 	c3d->texX = (int)(c3d->wallX * (double)c3d->wallT->width);
 	if ((c3d->side == 0 && c3d->rayDirX > 0) || (c3d->side == 1 && c3d->rayDirY < 0))
 		c3d->texX = c3d->wallT->width - c3d->texX - 1;
@@ -138,45 +138,56 @@ void	calculating_textured_rendering_wall(t_data *c3d, int x)
 	while (y < c3d->drawEnd)
 	{
 		c3d->texY = (int)(((y - SCREENHEIGHT / 2 + c3d->texHeight / 2) * c3d->wallT->height) / c3d->texHeight);
-		int color = c3d->wallT->addr[c3d->texY * c3d->wallT->width + c3d->texX];
-		c3d->image->addr[y * SCREENWIDTH + x] = color;
+		c3d->color = c3d->wallT->addr[c3d->texY * c3d->wallT->width + c3d->texX];
+		c3d->image->addr[y * SCREENWIDTH + x] = c3d->color;
 		y++;
 	}
 }
 
-void raycasting_core(t_data *c3d)
+void	initializing_parameters_raycasting(t_data *c3d, int x)
 {
-	int x;
+	c3d->cameraX = 2 * x / (double)SCREENWIDTH - 1;
+	c3d->rayDirX = c3d->dirX + c3d->planeX * c3d->cameraX;
+	c3d->rayDirY = c3d->dirY + c3d->planeY * c3d->cameraX;
+	c3d->mapX = (int)c3d->posX;
+	c3d->mapY = (int)c3d->posY;
+	if (c3d->rayDirX == 0)
+		c3d->deltaDistX = 1e30;
+	else
+		c3d->deltaDistX = fabs(1 / c3d->rayDirX);
+	if (c3d->rayDirY == 0)
+		c3d->deltaDistY = 1e30;
+	else
+		c3d->deltaDistY = fabs(1 / c3d->rayDirY);
+	c3d->hit = 0;
+}
+
+void	raycasting_core(t_data *c3d)
+{
+	int	x;
 
 	x = 0;
-    while (x < SCREENWIDTH)
-    {
-        c3d->cameraX = 2 * x / (double)SCREENWIDTH - 1;
-        c3d->rayDirX = c3d->dirX + c3d->planeX * c3d->cameraX;
-        c3d->rayDirY = c3d->dirY + c3d->planeY * c3d->cameraX;
-        c3d->mapX = (int)c3d->posX;
-        c3d->mapY = (int)c3d->posY;
-        c3d->deltaDistX = (c3d->rayDirX == 0) ? 1e30 : fabs(1 / c3d->rayDirX);
-        c3d->deltaDistY = (c3d->rayDirY == 0) ? 1e30 : fabs(1 / c3d->rayDirY);
-        c3d->hit = 0;
+	while (x < SCREENWIDTH)
+	{
+		initializing_parameters_raycasting(c3d, x);
 		determine_ray_intersects_distances(c3d);
 		hit_wall(c3d);
 		calculating_parameters_rendering_wall(c3d);
 		side_of_the_wall(c3d);
 		calculating_textured_rendering_wall(c3d, x);
 		x++;
-    }
+	}
 }
 
-void raycasting(t_data *c3d)
+void	raycasting(t_data *c3d)
 {
 	c3d->image = malloc(sizeof(t_img));
-    c3d->image->img = mlx_new_image(c3d->mlx, SCREENWIDTH, SCREENHEIGHT);
-    c3d->image->addr = (int *)mlx_get_data_addr(c3d->image->img, &c3d->image->bits_per_pixel, &c3d->image->line_length, &c3d->image->endian);
+	c3d->image->img = mlx_new_image(c3d->mlx, SCREENWIDTH, SCREENHEIGHT);
+	c3d->image->addr = (int *)mlx_get_data_addr(c3d->image->img, &c3d->image->bits_per_pixel, &c3d->image->line_length, &c3d->image->endian);
 	floor_ceiling(c3d);
 	raycasting_core(c3d);
-    mlx_put_image_to_window(c3d->mlx, c3d->win, c3d->image->img, 0, 0);
+	mlx_put_image_to_window(c3d->mlx, c3d->win, c3d->image->img, 0, 0);
 	mlx_destroy_image(c3d->mlx, c3d->image->img);
 	free(c3d->image);
-    bear_claws(c3d);
+	bear_claws(c3d);
 }
